2ec6983f1649db0318bb41afaf41facbfe4f36e8
David <hoang364.vn@gmail.com>
2024-10-28 15:56:26 -0700

    Setup pins for Ultrasonic Distance Sensor and Buttons

---
+++ Python Scripts/main.py
@@ -0,0 +1,20 @@
+import A6000_Control
+
+print('Please connect and switch on your camera')
+
+while True:
+    try:
+        # Initialize the camera
+        camera = gp.Camera()
+        camera.init()
+    except gp.GPhoto2Error as ex:
+        if ex.code == gp.GP_ERROR_MODEL_NOT_FOUND:
+            # No camera detected, try again in 2 seconds
+            print("Camera not found. Retrying in 2 seconds...")
+            time.sleep(2)
+            continue
+        # Raise any other errors that we can't handle
+        raise
+    # Operation completed successfully, exit loop
+    print("Camera initialized successfully.")
+    break
